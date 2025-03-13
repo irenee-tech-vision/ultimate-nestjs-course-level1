6e8b8c1f789049c373c1051b759c263ea5dc24d2
ireneea <20843309+ireneea@users.noreply.github.com>
2025-03-13 18:39:44 +0000

    feat: 0601 - Advanced DI - Class Provider

--- src/habits/habits.module.ts
+++ src/habits/habits.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { HabitsController } from './controllers/habits.controller';
-import { HabitsService } from './services/habits.service';
-import { InMemoryHabitsRepository } from './repositories/in-memory-habits.repository';
 import { InMemoryDbModule } from '../in-memory-db/in-memory-db.module';
+import { HabitsController } from './controllers/habits.controller';
+import { InMemoryHabitsRepository } from './repositories/in-memory-habits.repository';
+import { HabitsService } from './services/habits.service';
 
 @Module({
   imports: [InMemoryDbModule],
@@ -10,4 +10,3 @@ import { InMemoryDbModule } from '../in-memory-db/in-memory-db.module';
   providers: [HabitsService, InMemoryHabitsRepository],
 })
 export class HabitsModule {}
-
