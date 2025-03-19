79e5bf32b7f9275a16dcb19363736fd67634e5d9
ireneea <20843309+ireneea@users.noreply.github.com>
2025-03-19 07:36:17 +0000

    bugfix: 0902 - fixed mapUpdateUserDtoToUpdateUserInput

--- src/users/controllers/mappers/map-update-user-dto-to-update-user-input.ts
+++ src/users/controllers/mappers/map-update-user-dto-to-update-user-input.ts
@@ -1,17 +1,31 @@
 import { UpdateUserInput } from '../../services/models/update-user.input';
 import { UpdateUserDto } from '../dto/update-user.dto';
 
+const removeUndefinedValues = <T extends Record<string, any>>(
+  obj: T,
+): Partial<T> =>
+  Object.entries(obj)
+    .filter(([_, value]) => value !== undefined)
+    .reduce((acc, [key, value]) => {
+      acc[key] = value;
+      return acc;
+    }, {});
+
 export const mapUpdateUserDtoToUpdateUserInput = (
   id: number,
   updateUserDto: UpdateUserDto,
 ): UpdateUserInput => {
-  return {
+  const cleanData = removeUndefinedValues<Partial<UpdateUserInput>>({
     username: updateUserDto.username,
     email: updateUserDto.email,
     password: updateUserDto.password,
     firstName: updateUserDto.firstName,
     lastName: updateUserDto.lastName,
     dateOfBirth: updateUserDto.dateOfBirth,
+  });
+
+  return {
+    ...cleanData,
     userId: id,
   };
 };
