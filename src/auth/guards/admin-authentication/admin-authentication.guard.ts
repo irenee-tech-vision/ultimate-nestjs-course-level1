b23bf225079032078d76e879320b2a03663b7f0a
ireneea <20843309+ireneea@users.noreply.github.com>
2025-03-20 16:46:43 +0000

    feat: 1001 - Security - API Key - Authentication - Auth guard logic

--- src/auth/guards/admin-authentication/admin-authentication.guard.ts
+++ src/auth/guards/admin-authentication/admin-authentication.guard.ts
@@ -1,4 +1,10 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
 import { Observable } from 'rxjs';
 
 @Injectable()
@@ -6,6 +12,13 @@ export class AdminAuthenticationGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    const headers = request.headers;
+
+    if (headers['x-api-key'] !== '1234567890') {
+      throw new UnauthorizedException('Invalid API Key');
+    }
+
     return true;
   }
 }
