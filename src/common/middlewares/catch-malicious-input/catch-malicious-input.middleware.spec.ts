381e8a87fbc8d5747335ee18b123d6fbaeb55742
ireneea <20843309+ireneea@users.noreply.github.com>
2025-03-23 16:35:57 +0000

    feat: 1102 - Testing - NestJS Classes

--- src/common/middlewares/catch-malicious-input/catch-malicious-input.middleware.spec.ts
+++ src/common/middlewares/catch-malicious-input/catch-malicious-input.middleware.spec.ts
@@ -1,5 +1,71 @@
+import { ConfigService } from '@nestjs/config';
+import { AppConfigService } from '../../../app-config/app-config.service';
+import {
+  createRequest,
+  createResponse,
+  MockRequest,
+  MockResponse,
+} from 'node-mocks-http';
+import { CatchMaliciousInput } from './catch-malicious-input.middleware';
+import { HttpStatus } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { AppConfigModule } from '../../../app-config/app-config.module';
+
 describe('CatchMaliciousInput', () => {
+  let middleware: CatchMaliciousInput;
+  let appConfigService: AppConfigService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [CatchMaliciousInput],
+      imports: [AppConfigModule],
+    }).compile();
+
+    middleware = moduleRef.get<CatchMaliciousInput>(CatchMaliciousInput);
+    appConfigService = moduleRef.get<AppConfigService>(AppConfigService);
+
+    jest.resetAllMocks();
+  });
+
+  it('should be defined', () => {
+    expect(middleware).toBeDefined();
+  });
+
   describe('use', () => {
-    it('should return "BAD_REQUEST" if body is too large', () => {});
+    describe('when body is too large', () => {
+      let response: MockResponse<any>;
+      let request: MockRequest<any>;
+      let next: jest.Mock;
+
+      beforeEach(() => {
+        // Arrange
+        jest.spyOn(appConfigService, 'maxBodySize', 'get').mockReturnValue(10);
+
+        request = createRequest({
+          method: 'POST',
+          body: { name: 'test', description: 'a' },
+        });
+        response = createResponse();
+        next = jest.fn();
+
+        // Act
+        middleware.use(request, response, next);
+      });
+
+      it('should set response status code to "BAD_REQUEST"', () => {
+        expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST);
+      });
+
+      it('should set response data object', () => {
+        expect(response._getData()).toEqual({
+          message: 'Request body exceeds size limit',
+          statusCode: HttpStatus.BAD_REQUEST,
+        });
+      });
+
+      it('should not call the next middleware', () => {
+        expect(next).not.toHaveBeenCalled();
+      });
+    });
   });
 });
