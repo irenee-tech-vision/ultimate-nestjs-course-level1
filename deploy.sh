1c3a01861da1ebfe5c3526379a53ea9642ea3f80
ireneea <20843309+ireneea@users.noreply.github.com>
2025-04-10 20:15:31 +0100

    feat: remove nvm from the deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 SERVICE_NAME=${SERVICE_NAME:-habits-tracker-api}
 
