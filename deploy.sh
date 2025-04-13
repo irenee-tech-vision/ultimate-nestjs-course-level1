e51c485d439986312ed46a793049894003ded1e8
ireneea <20843309+ireneea@users.noreply.github.com>
2025-04-13 20:07:44 +0100

    feat: added github action

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+: "${LOCAL_KEY_PATH:?LOCAL_KEY_PATH not set}"
+: "${REMOTE_USER:?REMOTE_USER not set}"
+: "${REMOTE_HOST:?REMOTE_HOST not set}"
+: "${REMOTE_APP_PATH:?REMOTE_APP_PATH not set}"
 
-SERVICE_NAME=${SERVICE_NAME:-habits-tracker-api}
 
-echo "🔄 Pulling latest changes..."
-git pull origin master
+if [ "$SKIP_GIT" != "true" ]; then
+  echo "🔄 Pulling latest changes..."
+  git checkout master
+  git pull origin master
+fi
 
 echo "📦 Installing dependencies..."
 npm install
@@ -14,5 +18,25 @@ npm install
 echo "🔨 Building app..."
 npm run build
 
-echo "🚀 Restarting app: $SERVICE_NAME"
-pm2 restart "$SERVICE_NAME"
+echo "🚀 Sending files to server"
+rsync -avz \
+  --exclude=".git" \
+  --exclude="node_modules" \
+  --exclude=".env" \
+  -e "ssh -i $LOCAL_KEY_PATH" \
+  . $REMOTE_USER@$REMOTE_HOST:$REMOTE_APP_PATH
+
+echo "✅ Files synced!"
+
+
+ssh -i "$LOCAL_KEY_PATH" "$REMOTE_USER@$REMOTE_HOST" << EOF
+  export NVM_DIR="\$HOME/.nvm"
+  [ -s "\$NVM_DIR/nvm.sh" ] && \. "\$NVM_DIR/nvm.sh"
+  echo "📦 Installing prod dependencies..."
+  cd $REMOTE_APP_PATH
+  npm ci --omit=dev
+  echo "🔁 Restarting app with PM2..."
+  pm2 restart habits-tracker-api
+EOF
+
+echo "🎉 Deployment complete!"
