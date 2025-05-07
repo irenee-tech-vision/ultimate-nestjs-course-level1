e8edb72d1a52bef48a3e32707fda192fecae58ca
ireneea <20843309+ireneea@users.noreply.github.com>
2025-05-07 11:22:32 +0100

    feat: extend default logger

--- src/logger/amazing-logger.ts
+++ src/logger/amazing-logger.ts
@@ -1,44 +1,27 @@
-import { Injectable, LoggerService } from '@nestjs/common';
+import { ConsoleLogger, Injectable, LogLevel } from '@nestjs/common';
 import { AppConfigService } from '../app-config/app-config.service';
 
 @Injectable()
-export class AmazingLogger implements LoggerService {
-  constructor(private readonly configService: AppConfigService) {}
+export class AmazingLogger extends ConsoleLogger {
+  constructor(private readonly configService: AppConfigService) {
+    const maxLogLevel: LogLevel = configService.logLevel || 'debug';
+    const allLevels: LogLevel[] = [
+      'fatal',
+      'error',
+      'warn',
+      'log',
+      'debug',
+      'verbose',
+    ];
 
-  private shouldLog(level: string): boolean {
-    const levels = ['fatal', 'error', 'warn', 'log', 'debug', 'verbose'];
+    const currentLevelIndex = allLevels.indexOf(maxLogLevel);
+    const maxLogLevelIndex = allLevels.indexOf(maxLogLevel);
+    const logLevels = allLevels.slice(currentLevelIndex, maxLogLevelIndex + 1);
 
-    const currentLevelIndex = levels.indexOf(this.configService.logLevel);
-    const messageLevelIndex = levels.indexOf(level);
-
-    return messageLevelIndex <= currentLevelIndex;
-  }
-
-  log(message: any, ...optionalParams: any[]) {
-    if (this.shouldLog('log')) {
-      console.log('LOG:', message, ...optionalParams);
-    }
-  }
-
-  fatal(message: any, ...optionalParams: any[]) {
-    if (this.shouldLog('fatal')) {
-      console.error('FATAL:', message, ...optionalParams);
-    }
-  }
-
-  error(message: any, ...optionalParams: any[]) {
-    console.error('ERROR:', message, ...optionalParams);
-  }
-
-  warn(message: any, ...optionalParams: any[]) {
-    console.warn('WARN:', message, ...optionalParams);
-  }
-
-  debug?(message: any, ...optionalParams: any[]) {
-    console.debug('DEBUG:', message, ...optionalParams);
-  }
-
-  verbose?(message: any, ...optionalParams: any[]) {
-    console.log('VERBOSE:', message, ...optionalParams);
+    super({
+      logLevels,
+      json: true,
+      colors: true,
+    });
   }
 }
