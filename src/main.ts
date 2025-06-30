5bd6c20a3af494395d0760a0e9bbfc8c5d7d07e1
ireneea <20843309+ireneea@users.noreply.github.com>
2025-06-30 13:14:09 +0100

    feat: initialise swagger module

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import helmet from 'helmet';
 import { Logger as PinoLogger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 import { DbType } from './db-type.enum';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const appDataDb = (process.env.APP_DATA_DB as DbType) ?? DbType.IN_MEMORY;
@@ -31,6 +32,14 @@ async function bootstrap() {
   );
 
   app.enableShutdownHooks();
+
+  const config = new DocumentBuilder()
+    .setTitle("Habits Tracker API")
+    .setDescription("API that helps users track their habits")
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup("api", app, documentFactory())
 
   await app.listen(process.env.PORT ?? 3000);
 }
